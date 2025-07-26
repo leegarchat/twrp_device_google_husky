38cc59c0436529c184e894fafb74dc0e7329ff6f
alexey <leegarchat@gmail.com>
2025-07-26 08:40:05 +0300

    Fix load ofox with sultankernel(non gki kernels)

--- repack_image.sh
+++ repack_image.sh
@@ -4,6 +4,7 @@ SCRIPT_PATH="$(readlink -f "$0")"
 MAIN_PATH=$(dirname $SCRIPT_PATH)
 FOX_PATH=$(readlink -f "$MAIN_PATH/../../..")
 device="$(basename $MAIN_PATH)"
+
 cd $FOX_PATH/out/target/product/$device/
 mkdir repack_boot
 cd repack_boot
@@ -16,10 +17,11 @@ fi
 magiskboot cpio vendor_ramdisk/$name_ramdisk.cpio \
     "add 644 /twres/pages/advanced.xml $MAIN_PATH/fox_mod_ui/advanced.xml" \
     "add 644 /twres/pages/install.xml $MAIN_PATH/fox_mod_ui/install.xml"
+rm dtb
 magiskboot repack ../OrangeFox-R11.3-Unofficial-$device.img
 cp -f new-boot.img ../OrangeFox-R11.3-Unofficial-$device.img
 
-DATE=$(date +"%Y-%d-%m")
+DATE=$(date +"%Y-%d-%m_%H-%M")
 
 if [ "$1" == "move" ] ; then
     cp new-boot.img $MAIN_PATH/releases/OrangeFox-R11.3-Unofficial-$device-$DATE.img
